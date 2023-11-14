d99ab15c64e3aec29ea1f61f99631dba1dffbb31
juleneuribarri <julene.uribarri@opendeusto.es>
2023-11-14 11:48:14 +0100

    cambio_4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
-#include "mbed.h"
 #include "datos.h"
 #include "math.h"
-
+#include "mbed.h"
 
 #define longitudTrama 200
-#define fs (50*20)
+#define fs (50 * 20)
 
 Timer timer;
 float resultado;
 int tiempo;
-
 
 struct estructuraMedidas {
   float vrms;
@@ -59,10 +57,12 @@ int main() {
 
 // Esta función calcula el valor RMS
 float calcularRMS(uint16_t *datos, int longitud) {
-float rms = 0;
-float constante =  800.0 / 65536; // divido al revés para que sea más rápido y así luego multiplico
+  float rms = 0;
+  float constante =
+      800.0 /
+      65536; // divido al revés para que sea más rápido y así luego multiplico
   for (int i = 0; i < longitud; i++) {
-    float datoV = ((datos[i]) * constante) - 400.0; 
+    float datoV = ((datos[i]) * constante) - 400.0;
     rms += datoV * datoV;
   }
   rms = sqrt(rms / longitud);
@@ -78,33 +78,34 @@ void calcularDatos(uint16_t *datosV, uint16_t *datosI, int longitud,
   float P = 0;
   float S, Q, FA, E;
 
-// vuelvo a declarar porque si hago como global tarda más
-float constante =  800.0 / 65536; // divido al revés para que sea más rápido y así luego multiplico
-float constante_2 =  5.0 / 65536;
+  // vuelvo a declarar porque si hago como global tarda más
+  float constante =
+      800.0 /
+      65536; // divido al revés para que sea más rápido y así luego multiplico
+  float constante_2 = 5.0 / 65536;
 
   for (int i = 0; i < longitud; i++) {
     float datoV = ((datosV[i]) * constante) - 400.0;
     Vrms += datoV * datoV;
     float datoI = ((datosI[i]) * constante_2) - 2.5;
-    Irms += datoI *datoI;
+    Irms += datoI * datoI;
     P += datoV * datoI;
-
   }
 
-// FUNCION SQRT?
-// SE PUEDE HACER COMO EN MATLAB LO DE METER A LA DCHA?
+  // FUNCION SQRT? no hace falta
+  // SE PUEDE HACER COMO EN MATLAB LO DE METER A LA DCHA? format document
   Vrms = sqrt(Vrms / longitud);
   Irms = sqrt(Irms / longitud);
   P = P / longitud;
   S = Vrms * Irms;
-  Q = sqrt(S*S - P*P);
+  Q = sqrt(S * S - P * P);
   FA = P / S;
-  E = P/fs*longitudTrama/60/60/1000;
+  E = P / fs * longitudTrama / 60 / 60 / 1000;
   medidas->vrms = Vrms;
   medidas->irms = Irms;
   medidas->potenciaActiva = P;
   medidas->potenciaAparente = S;
   medidas->potenciaReactiva = Q;
   medidas->factorDePotencia = FA;
-  medidas->energiaConsumida+=E;
+  medidas->energiaConsumida += E;
 }
